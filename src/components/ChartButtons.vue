6c4beb6c0eb0b294dfeed386597dcc1487ee2fdf
Joshua Siegal <joshuasiegal@yahoo.com>
2020-07-05 16:22:32 -0500

    update default styling and behaviors

--- src/components/ChartButtons.vue
+++ src/components/ChartButtons.vue
@@ -2,31 +2,40 @@
   <div id="chart-buttons">
     
     <div id="state-grid">
-      <button v-for="state in states" :class="{'selected':(isStateSelected(state))}" @click="toggleSelectState(state)">{{state}}</button>
+      <button v-for="state in states" :class="{'selected':(isStateSelected(state)), 'default-active':(isStateActiveDefault(state))}" @click="toggleSelectState(state)">{{state}}</button>
     </div>
 
     <div id="conv-regions">
-      <button @click="selectRegion('pacific')" class="button-select-west button-pill button-pill-left">Pacific</button>
-      <button @click="selectRegion('west')" class="button-select-west button-pill">West</button>
-      <button @click="selectRegion('southwest')" class="button-select-southwest button-pill">Southwest</button>
-      <button @click="selectRegion('plains')" class="button-select-southwest button-pill">Plains</button>
-      <button @click="selectRegion('midwest')" class="button-select-midwest button-pill">Midwest</button>
-      <button @click="selectRegion('south')" class="button-select-west button-pill">South</button>
-      <button @click="selectRegion('southeast')" class="button-select-west button-pill">Southeast</button>
-      <button @click="selectRegion('newengland')" class="button-select-west button-pill">New England</button>
-      <button @click="selectRegion('northeast')" class="button-select-west button-pill">NorthEast</button>
-      <button @click="selectRegion('territories')" class="button-select-west button-pill button-pill-right">Territories</button>
+      <div class="mobile-set">
+        <button @click="selectRegion('pacific')" class="button-select-west button-pill">Pacific</button>
+        <button @click="selectRegion('west')" class="button-select-west button-pill">West</button>
+        <button @click="selectRegion('southwest')" class="button-select-southwest button-pill">Southwest</button>
+        <button @click="selectRegion('plains')" class="button-select-southwest button-pill">Plains</button>
+        <button @click="selectRegion('midwest')" class="button-select-midwest button-pill">Midwest</button>
+        <button @click="selectRegion('south')" class="button-select-west button-pill">South</button>
+        <button @click="selectRegion('southeast')" class="button-select-west button-pill">Southeast</button>
+        <button @click="selectRegion('newengland')" class="button-select-west button-pill">New England</button>
+        <button @click="selectRegion('northeast')" class="button-select-west button-pill">NorthEast</button>
+        <button @click="selectRegion('territories')" class="button-select-west button-pill">Territories</button>
+      </div>
     </div>
 
     <div id="conv-settings">
-      <button @click="clearSelections" class="button-clear-selections button-pill button-pill-left">Clear Selections</button>
-      <button @click="handleDefaultStates" :class="{'disabled':!selectedStates.length && !defaultStates.length}" class="button-defaults button-pill">
-        <span v-text="(defaultStates.length) ? 'Clear Default' : 'Set As Default' " ></span>
-        <span class="verification verification-default-states" :class="{'active':defaultStatesSet}">You're Set!</span>
-        <span class="verification verification-clear-default-states" :class="{'active':defaultStatesClear}">Cleared!</span>
-      </button>
-      <button @click="showDefault" :class="{'disabled':!defaultStates.length}" class="button-show-default button-pill">Show Default</button>
-      <button @click="selectAllStates" class="button-select-all button-pill button-pill-right">Select All States</button>
+      <div class="mobile-set">
+        <button @click="clearSelections" class="button-clear-selections button-pill">Clear Selections</button>
+        <button @click="selectAllStates" class="button-select-all button-pill">Select All States</button>
+        <button @click="copyLink" class="button-copy link button-pill">Copy Link
+          <span class="verification verification-copy link" :class="{'active':linkCopied}">Copied!</span>
+        </button>
+      </div>
+      <div class="mobile-set"> 
+        <button @click="handleDefaultStates" :class="{'disabled':!selectedStates.length && !defaultStates.length}" class="button-defaults button-pill">
+          <span v-text="(defaultStates.length) ? 'Clear Default' : 'Set As Default' " ></span>
+          <span class="verification verification-default-states" :class="{'active':defaultStatesSet}">You're Set!</span>
+          <span class="verification verification-clear-default-states" :class="{'active':defaultStatesClear}">Cleared!</span>
+        </button>
+        <button @click="showDefault" :class="{'disabled':!showDefaultEnabled, 'active':showDefaultActive}" class="button-show-default button-pill">Show Default</button>
+      </div>
     </div>
 
     <div id="conv-select-cont">
@@ -50,6 +59,14 @@
 
 <style scoped lang="scss">
 
+  @mixin left-pill {
+    border-radius:4px 0 0 4px;
+  }
+
+  @mixin right-pill {
+    border-radius:0 4px 4px 0;
+  }
+
   #state-grid {
     margin:24px auto;
     width:72vw;
@@ -67,6 +84,10 @@
       &.selected {
         background-color:#CEA3A3;
       }
+
+      &.selected.default-active {
+        background-color:#A3AACE;
+      }
     }
   }
 
@@ -78,6 +99,7 @@
     display:block;
     padding:3px 6px;
     top:0px;
+    left:0px;
     width:100%;
     display:none;
     border-radius:2px;
@@ -87,9 +109,6 @@
       animation:fadeInAndRiseOut 1.5s ease-out forwards;
     }
 
-    &.verification-default-states {
-      left:0px;
-    }
   }
 
   #conv-regions,
@@ -101,14 +120,23 @@
       margin-bottom:24px;
       position:relative;
       overflow:visible;
+    }
 
-      &.button-pill-left {
-        border-radius:4px 0 0 4px;
+    .mobile-set {
+      display:inline;
+
+      &:first-child button:first-child {
+        @include left-pill;
       }
 
-      &.button-pill-right {
-        border-radius:0 4px 4px 0;
+      &:last-child button:last-child {
+        @include right-pill;
       }
+    }
+
+    .button-show-default.disabled.active {
+      background-color:#A3AACE;
+      color:#FFFFFF;
     }
   }
 
@@ -138,11 +166,20 @@
       display:none;
     }
 
-/*    .button-pill-left,
-    .button-pill-right {
-      padding:12px;
-      display:inline;
-    }*/
+    #conv-settings {
+      .mobile-set {
+        display:block;
+
+        button:first-child {
+          @include left-pill;
+        }
+
+        button:last-child {
+          @include right-pill;
+        }
+      }
+
+    }
 
     #conv-select-cont {
       display:block;
@@ -169,11 +206,13 @@ export default {
   props:['selectedStates', 'states'],
 
   data: () => ({
-    statesForURL: [],
+    ANIMATION_TIME:1500,
+    statesForURL:[],
     selectedRegion:'',
     defaultStates:[],
     defaultStatesSet:false,
     defaultStatesClear:false,
+    linkCopied:false,
     regionStrings: {
       pacific: ['WA','OR','CA','HI','AK'], 
       west: ['NV','ID','MT','WY','CO','UT'],
@@ -223,7 +262,7 @@ export default {
         this.defaultStatesSet = true
         setTimeout(() => {
           this.defaultStatesSet = false
-        },1500)
+        },this.ANIMATION_TIME)
       } catch(e) {
        console.warn(e)
       }
@@ -236,20 +275,25 @@ export default {
         this.defaultStatesClear = true
         setTimeout(() => {
           this.defaultStatesClear = false
-        },1500)
+        },this.ANIMATION_TIME)
       } catch(e) {
         console.warn(e)
       }
     },
 
     showDefault() {
-      if (!this.defaultStates.length) { return }
+      if (!this.showDefaultEnabled) { return }
       this.statesForURL = [...this.defaultStates]
       this.updateURL()
     },
 
     isStateSelected(state) {
       return (this.selectedStates.indexOf(state) >= 0)
+    },
+
+    isStateActiveDefault(state) {
+      return  ( (this.defaultStates.indexOf(state) >= 0 ) &&
+              !this.showDefaultEnabled )
     },
 
     clearSelections() {
@@ -280,6 +324,13 @@ export default {
     selectRegion(region) {
       this.statesForURL = [...this.regionStrings[region]]
       this.updateURL()
+    },
+
+    copyLink() {
+      this.linkCopied = true
+      setTimeout(() => {
+        this.linkCopied = false
+      },this.ANIMATION_TIME)
     }
 
   },
@@ -293,7 +344,13 @@ export default {
   },
 
   computed: {
-
+    showDefaultEnabled() {
+      if (!this.defaultStates.length) { return false }
+      return (this.selectedStates.join('-') != this.defaultStates.join('-'))
+    },
+    showDefaultActive() {
+      return (!this.showDefaultEnabled && this.selectedStates.length && this.defaultStates.length)
+    }
   },
 
   created() {
